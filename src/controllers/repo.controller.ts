7395f29e59e71a506003aaab07020858092bae69
vishal9198 <vkgupta4558@gmail.com>
2026-06-20 18:25:49 +0530

    add repo for metadata fetching

--- src/controllers/repo.controller.ts
+++ src/controllers/repo.controller.ts
@@ -1,19 +1,27 @@
 import { Router } from "express";
 import { Request, Response } from "express";
-import { parseGitHubRepoUrl } from "../services/github.services";
+import {
+  parseGitHubRepoUrl,
+  fetchRepoMetadata,
+} from "../services/github.services";
 
-export const analyzeRepoController = (req: Request, res: Response) => {
+export const analyzeRepoController = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
   try {
     const { repoUrl } = req.body;
 
     const parsedRepo = parseGitHubRepoUrl(repoUrl);
+    const metadata = await fetchRepoMetadata(parsedRepo.owner, parsedRepo.repo);
 
     res.json({
       success: true,
-      data: parsedRepo,
+      message: "Repository metadata fetched successfully",
+      data: metadata,
     });
   } catch (error) {
-    res.status(400).json({
+    res.status(500).json({
       success: false,
       message: error instanceof Error ? error.message : "Something went wrong",
     });
--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,12 @@ import repoRoutes from "./routes/repo.routes";
 import { config } from "./config";
 
 const app = express();
-const PORT = 3000;
+
 app.use(cors());
 app.use(express.json());
 app.use("/api/health", healthRoutes);
 
 app.use("/api/repos", repoRoutes);
-app.listen(PORT, () => {
+app.listen(config.PORT, () => {
   console.log(`server is running on ${config.PORT}`);
 });
